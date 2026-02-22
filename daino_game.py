222943523de116e0ca7782883658edacf699b59a
shahriyar <shahriyarshokri2007@gmail.com>
2026-02-23 01:07:35 +0330

    added enemy and jump

--- daino_game.py
+++ daino_game.py
@@ -1,8 +1,11 @@
 import pygame
+import time
 
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
+font = pygame.font.Font(None, 36)
+big_font = pygame.font.Font(None, 74)
 
 pygame.display.set_caption('Dinogame')
 icon = pygame.image.load('daino.png')
@@ -11,24 +14,88 @@ pygame.display.set_icon(icon)
 player_img = pygame.image.load('daino.png')
 playerX=1
 playerY=300
+playerY_ground=300
 
+enemy_img = pygame.image.load('enemy.png')
+enemyX=800
+enemyY=300
+
+enemy2_img = pygame.image.load('enemy2.png')
+enemy2X=400
+enemy2Y=300
+
+
+clock = pygame.time.Clock()
+FPS = 60
+
+start=time.time()
+def show_time(seconds):
+    minutes = int(seconds // 60)
+    seconds = int(seconds % 60)
+    return f"{minutes:02d}:{seconds:02d}"
 
 def player(x,y):
     screen.blit(player_img, (x,y))
 
-counter=0
+def enemy(x,y):
+    screen.blit(enemy_img, (x,y))
+
+def enemy2(x,y):
+    screen.blit(enemy2_img, (x,y))
+
+
+score = 0
+is_jumping = False
+jump_velocity = 0
+gravity = 0.5
+jump_strength = -10 
 running=True
 while running:
+    clock.tick(FPS)
 
-    screen.fill((30,30,30))
-    playerX += 0.1
-    counter+=1
-    if counter == 7914:
-        playerX == 1
-        continue
+    all_time=time.time() -start
+    screen.fill((30, 30, 30))
+    timer_text = font.render(show_time(all_time), True, (0, 255, 0))
+    screen.blit(timer_text, (375, 20))
+
+    enemyX -= 6
+    enemy2X -= 6
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not is_jumping:
+                is_jumping = True
+                jump_velocity = jump_strength
+    if is_jumping:
+        playerY += jump_velocity
+        jump_velocity  += gravity
+
+        if playerY >= playerY_ground:
+            playerY = playerY_ground
+            is_jumping = False
+            jump_velocity = 0
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >=750:
+        playerX = 0
+
+    if enemyX <=0:
+        enemyX = 750
+    elif enemyX >=750:
+        enemyX = 0
+
+    if enemy2X <=0:
+        enemy2X = 750
+    elif enemy2X >=750:
+        enemy2X = 0
     
     player(playerX,playerY)
+    enemy(enemyX,enemyY)
+    enemy2(enemy2X,enemy2Y)
+    end=time.time()
     pygame.display.update()
+
+pygame.quit()
